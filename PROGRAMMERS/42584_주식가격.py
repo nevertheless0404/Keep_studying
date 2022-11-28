c7de56b0e2496c674189686a82d4d7276e50ff1f
nevertheless0404 <nevertheless.hi@gmail.com>
2022-11-28 19:31:44 +0900

    1128

---
+++ PROGRAMMERS/42584_주식가격.py
@@ -0,0 +1,16 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/42584
+
+from collections import deque
+
+def solution(prices):
+    queue = deque(prices)
+    answer = []
+    while queue:
+        price = queue.popleft()
+        sec = 0
+        for q in queue:
+            sec += 1
+            if price > q:
+                break
+        answer.append(sec)
+    return answer
