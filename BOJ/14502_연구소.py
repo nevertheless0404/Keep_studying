9957be8e42ef2d207b8c9c4cd4dc382fc9de4cd3
nevertheless0404 <nevertheless.hi@gmail.com>
2022-12-22 21:10:31 +0900

    수정해야됌

--- BOJ/14502_연구소.py
+++ BOJ/14502_연구소.py
@@ -1,1 +1,23 @@
 # https://www.acmicpc.net/problem/14502
+
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
+visit = [[0] * m for _ in range(n)]
+
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+
+
+def virus(x, y):
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        # 나가지 않게 하기!
+        if 0 <= nx < n and 0 <= ny < m and visit[nx][ny] == 0:
+            visit[nx][ny] = 2
+            virus(nx, ny)
