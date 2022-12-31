fce1ed6cbb254b43df6dbbc56a66e1ff5ce7832a
YuYeong <108647801+nevertheless0404@users.noreply.github.com>
2022-12-31 15:17:34 +0900

    [Gold V] Title: 로봇 청소기, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -7,31 +7,36 @@ visited = [[0]*m for _ in range(n)]
 dr = [-1, 0, 1, 0]
 dc = [0, 1, 0, -1]
 
+# 시작지 방문 체크 
 visited[r][c] = 1
 cnt = 1
 
 while True:
-    flag = 0            ## 아직 아무것도 청소 안했음!
-    for _ in range(4):  ## 4방향을 돈다!
-        d = (d+3) % 4   ## 왼쪽방향으로 한 칸 돌린다! 중요!!!!!1
+    # 아직 청소 안했음!
+    flag = 0           
+    for _ in range(4): 
+        # 왼쪽 방향으로 한칸 돌림!!!
+        d = (d + 3) % 4   
         nr = r + dr[d]
         nc = c + dc[d]
 
-        ## 범위 안에 들고, 빈 칸이고, 청소할 수 있다면!
-        ## 들려서 청소하고, 카운트하고, 현재 위치를 갱신하고, flag 변경!
+        # 범위 안에 들고, 빈칸이면 청소 할 수 있으면 
+        # 들려서 청소하고, 카운트!
+        # 현재위치 갱신, flag 변경 및 청소 
         if 0 <= nr < n and 0 <= nc < m and arr[nr][nc] == 0:
             if visited[nr][nc] == 0:
                 visited[nr][nc] = 1
                 cnt += 1
                 r = nr
                 c = nc
-                flag = 1        ## 청소 했다는 뜻
+                flag = 1        
                 break
-
-    if flag == 0:               ## 위의 for문에 들어가지 못했을 때
-        ## 즉 네 방향 모두 청소를 할 수 없을 때
-        ## 후진 했을 때 벽이면 break
-        ## 만약 뒤가 벽이 아니라면! 그 위치를 다시 갱신!!!
+   
+    if flag == 0:
+        # 네 방향 모두 청소를 할 수 없을 때 
+        # 후친 했을 때 벽이면 break
+        # 만약 뒤가 벽이 아니라면!
+        # 그 위치를 다시 갱신!!
         if arr[r-dr[d]][c-dc[d]] == 1:
             print(cnt)
             break
