9a4a503e19b5762c555253a09174cc092f70c402
YuYeong <108647801+nevertheless0404@users.noreply.github.com>
2023-01-03 18:11:14 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.21 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -0,0 +1,17 @@
+def solution(survey, choices):
+    answer = ''
+    check = {"R":0, "T":0, "C":0, "F":0, "J":0, "M":0, "A":0, "N":0, }
+    for i in range(len(choices)):
+        l = survey[i][0]
+        r = survey[i][1]
+        if choices[i] == 4:
+            continue
+        elif choices[i] < 4:
+            check[l] += (4 - choices[i])
+        else:
+            check[r] += (choices[i] - 4)
+    answer += "R" if check["R"] >= check["T"] else "T"
+    answer += "C" if check["C"] >= check["F"] else "F"
+    answer += "J" if check["J"] >= check["M"] else "M"
+    answer += "A" if check["A"] >= check["N"] else "N"
+    return answer
