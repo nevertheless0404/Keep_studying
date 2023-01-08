d0a3a18b51a4551d81524081b700fb0b22e687ef
YuYeong <108647801+nevertheless0404@users.noreply.github.com>
2023-01-08 21:27:05 +0900

    [Gold V] Title: 4연산, Time: 64 ms, Memory: 34104 KB -BaekjoonHub

---
+++ 백준/Gold/14395. 4연산/4연산.py
@@ -0,0 +1,47 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+s, t = map(int, input().split())
+# 순서가 정해져 있음
+oper = ["*", "+", "-", "/"]
+
+
+def bfs():
+    q = deque()
+    q.append((s, ""))
+    visit = []
+
+    while q:
+        cnt = 0
+        n, o = q.popleft()
+
+        for i in oper:
+            if i == "*":
+                cnt = n * n
+            elif i == "+":
+                cnt = n + n
+            elif i == "-":
+                cnt = n - n
+            # s 가 0이 아닐때만 사용 가능
+            elif i == "/" and n != 0:
+                cnt = int(n / n)
+
+
+            if 0 < n <= 10**9:
+                if cnt == t:
+                    print(o + i)
+                    return
+                if cnt not in visit:
+                    q.append((cnt, o + i))
+                    visit.append(cnt)
+
+    print(-1)
+    return
+
+
+if s == t:
+    print(0)
+else:
+    bfs()
