3e3143d02b5b413197dcd8247bf18e7e6fe777b6
YuYeong <108647801+nevertheless0404@users.noreply.github.com>
2023-01-10 16:47:37 +0900

    [Silver V] Title: 올림픽, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -9,9 +9,7 @@ for i in range(n):
     m = list(map(int, input().split()))
     medal.append(m)
 
-# 앞에 마이너스를 붙이지 않으면 등수가 거꾸로 나오기 때문에
-# 붙여줘야 함
-medal.sort(key=lambda x: (-x[1], -x[2], -x[3]))
+medal.sort(key=lambda x: (x[1], x[2], x[3]))
 
 for i in range(n):
     # 등수를 알고 싶은 국가 넣기
