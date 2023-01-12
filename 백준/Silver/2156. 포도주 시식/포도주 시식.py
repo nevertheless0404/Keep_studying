caa47a799c48269eaf789c55732efb75ef09de4f
YuYeong <108647801+nevertheless0404@users.noreply.github.com>
2023-01-12 23:33:37 +0900

    [Silver I] Title: 포도주 시식, Time: 44 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -0,0 +1,18 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+wine = [int(input()) for _ in range(n)]
+
+dp = [0, wine[0]]
+
+if n > 1:
+    dp.append(wine[0] + wine[1])
+
+for i in range(3, n + 1):
+    dp.append(
+        max(dp[i - 2] + wine[i - 1], dp[i - 1], dp[i - 3] + wine[i - 1] + wine[i - 2])
+    )
+
+print(dp[n])
