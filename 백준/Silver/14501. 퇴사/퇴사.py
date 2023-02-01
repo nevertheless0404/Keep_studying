4c8b151bd4a45221c58fea63772363c06666064d
YuYeong <108647801+nevertheless0404@users.noreply.github.com>
2023-02-01 16:14:20 +0900

    [Silver III] Title: 퇴사, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -0,0 +1,22 @@
+n = int(input())
+
+t = []
+p = []
+dp = [0 for _ in range(n + 1)]
+
+for _ in range(n):
+    T, P = map(int, input().split())
+    t.append(T)
+    p.append(P)
+
+# 뒤에서부터
+for i in range(n - 1, -1, -1):
+    # 상당에 필요한 일수가 퇴사일을 넘긴다면?
+    if t[i] + i > n:
+        dp[i] = dp[i + 1]
+
+    # 오늘 상담할 경우
+    else:
+        dp[i] = max(dp[i + 1], dp[t[i] + i] + p[i])
+
+print(dp[0])
