a11bca04c5b8920f91b0b6f72014d7c646177b87
nevertheless0404 <nevertheless.hi@gmail.com>
2023-02-05 18:05:59 +0900

    0205

---
+++ BOJ/13549_숨바꼭질3.py
@@ -0,0 +1,30 @@
+# https://www.acmicpc.net/problem/13549
+
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+
+def bfs():
+    q = deque([n])
+
+    while q:
+        x = q.popleft()
+        if x == k:
+            return visit[x]
+
+        for nx in (x - 1, x + 1, 2 * x):
+            if 0 <= nx < 100001 and not visit[nx]:
+                if nx == 2 * x and x != 0:
+                    visit[nx] = visit[x]
+                    q.appendleft(nx)
+                else:
+                    visit[nx] = visit[x] + 1
+                    q.append(nx)
+
+
+n, k = map(int, input().split())
+visit = [0] * 100001
+
+print(bfs())
