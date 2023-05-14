fcfb0538d40a92eab185741dde901a64ae99338b
YuYeong <108647801+nevertheless0404@users.noreply.github.com>
2023-05-15 08:43:09 +0900

    [Silver V] Title: 분수찾기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1193. 분수찾기/분수찾기.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+line = 1
+
+while n > line:
+    n -= line
+    line += 1
+    
+if line % 2 == 0:
+    a = n
+    b = line - n + 1
+    
+else:
+    a = line - n + 1
+    b = n
+
+print(a, '/', b, sep='')
